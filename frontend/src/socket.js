abd78183256df6266037aa9a3eba445f3b040f6f
Anurag <123239264+Anuragkumarbhot@users.noreply.github.com>
2026-03-16 03:46:49 +0530

    Update socket.js
     "connect backend"

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"
 
-const socket = io("https://ai-powerlifting-coach-backend.onrender.com")
+const socket = io("https://ai-powerlifting-backend.onrender.com")
 
 export default socket
